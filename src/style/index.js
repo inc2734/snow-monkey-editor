30619c85e0bc795b0f978f7baea4c59dc216c5c4
Takashi Kitajima <inc@2inc.org>
2020-01-16 11:08:10 +0900

    Add Post-it (Narrow) block style

--- src/style/index.js
+++ src/style/index.js
@@ -12,6 +12,7 @@ import fluidShape1 from './fluid-shape-1';
 import fluidShape2 from './fluid-shape-2';
 import fluidShape3 from './fluid-shape-3';
 import postIt from './post-it';
+import postItNarrow from './post-it-narrow';
 import shadowed from './shadowed';
 import speech from './speech';
 
@@ -23,5 +24,6 @@ fluidShape1.forEach( ( props ) => registerStyle( props ) );
 fluidShape2.forEach( ( props ) => registerStyle( props ) );
 fluidShape3.forEach( ( props ) => registerStyle( props ) );
 postIt.forEach( ( props ) => registerStyle( props ) );
+postItNarrow.forEach( ( props ) => registerStyle( props ) );
 shadowed.forEach( ( props ) => registerStyle( props ) );
 speech.forEach( ( props ) => registerStyle( props ) );
---
+++ src/style/post-it-narrow/settings.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import {
+	__,
+} from '@wordpress/i18n';
+
+export default {
+	name: 'sme-post-it-narrow',
+	label: __( 'Post-it (Narrow)', 'snow-monkey-editor' ),
+};
