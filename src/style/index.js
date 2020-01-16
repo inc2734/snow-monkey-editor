7fe73c98fb4b5ba57eba331ac921fd619200c425
Takashi Kitajima <inc@2inc.org>
2020-01-16 10:16:30 +0900

    Add speech block style

--- src/style/index.js
+++ src/style/index.js
@@ -12,6 +12,7 @@ import fluidShape1 from './fluid-shape-1';
 import fluidShape2 from './fluid-shape-2';
 import fluidShape3 from './fluid-shape-3';
 import shadowed from './shadowed';
+import speech from './speech';
 
 alert.forEach( ( props ) => registerStyle( props ) );
 alertSuccess.forEach( ( props ) => registerStyle( props ) );
@@ -21,3 +22,4 @@ fluidShape1.forEach( ( props ) => registerStyle( props ) );
 fluidShape2.forEach( ( props ) => registerStyle( props ) );
 fluidShape3.forEach( ( props ) => registerStyle( props ) );
 shadowed.forEach( ( props ) => registerStyle( props ) );
+speech.forEach( ( props ) => registerStyle( props ) );
---
+++ src/style/speech/settings.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import {
+	__,
+} from '@wordpress/i18n';
+
+export default {
+	name: 'sme-speech',
+	label: __( 'Speech', 'snow-monkey-editor' ),
+};
