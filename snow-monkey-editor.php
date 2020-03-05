459b8a2b53723c8ca96a21ba1065f4af41ec5a08
Takashi Kitajima <inc@2inc.org>
2020-03-05 10:32:30 +0900

    1.0.0

--- snow-monkey-editor.php
+++ snow-monkey-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey Editor
- * Version: 0.8.2
+ * Version: 1.0.0
  * Description: Extends gutenberg block editor
  * Author: inc2734
  * Author URI: https://2inc.org
