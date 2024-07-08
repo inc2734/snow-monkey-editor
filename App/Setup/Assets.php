c7b25786080e18e166cc6da510c9f38e78ded2d0
inc2734 <inc@2inc.org>
2024-07-08 14:33:50 +0900

    #113 Some updates

--- App/Setup/Assets.php
+++ App/Setup/Assets.php
@@ -17,7 +17,6 @@ class Assets {
 	public function __construct() {
 		add_action( 'enqueue_block_assets', array( $this, '_enqueue_block_assets' ) );
 		add_action( 'enqueue_block_editor_assets', array( $this, '_enqueue_block_editor_extension' ), 9 );
-		add_action( 'enqueue_block_editor_assets', array( $this, '_enqueue_block_editor_assets' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, '_wp_enqueue_scripts' ) );
 	}
 
@@ -32,6 +31,62 @@ class Assets {
 				array(),
 				filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/css/fallback.css' )
 			);
+		}
+
+		wp_enqueue_style(
+			'snow-monkey-editor',
+			SNOW_MONKEY_EDITOR_URL . '/dist/css/app.css',
+			array(),
+			filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/css/app.css' )
+		);
+
+		if ( is_admin() ) {
+			$asset = include SNOW_MONKEY_EDITOR_PATH . '/dist/js/editor.asset.php';
+			wp_enqueue_script(
+				'snow-monkey-editor@editor',
+				SNOW_MONKEY_EDITOR_URL . '/dist/js/editor.js',
+				$asset['dependencies'],
+				filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/js/editor.js' ),
+				array(
+					'in_footer' => false,
+					'strategy'  => 'defer',
+				)
+			);
+
+			wp_set_script_translations(
+				'snow-monkey-editor@editor',
+				'snow-monkey-editor',
+				SNOW_MONKEY_EDITOR_PATH . '/languages'
+			);
+
+			wp_enqueue_style(
+				'snow-monkey-editor@editor',
+				SNOW_MONKEY_EDITOR_URL . '/dist/css/editor.css',
+				array(),
+				filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/css/editor.css' )
+			);
+
+			$palette     = get_theme_support( 'editor-color-palette' );
+			$color_style = '';
+			if ( is_array( $palette ) && ! empty( $palette ) ) {
+				foreach ( $palette[0] as $color ) {
+					$color_style .= ':root .editor-styles-wrapper .has-' . esc_attr( $color['slug'] ) . '-color[class*="sme-"],:root .customize-control-sidebar_block_editor .has-' . esc_attr( $color['slug'] ) . '-color[class*="sme-"]{color:' . esc_attr( $color['color'] ) . '}';
+					$color_style .= ':root .editor-styles-wrapper .has-' . esc_attr( $color['slug'] ) . '-background-color[class*="sme-"],:root .customize-control-sidebar_block_editor .has-' . esc_attr( $color['slug'] ) . '-background-color[class*="sme-"]{background-color:' . esc_attr( $color['color'] ) . '}';
+				}
+				if ( $color_style ) {
+					wp_add_inline_style( 'snow-monkey-editor@editor', $color_style );
+				}
+			}
+
+			$current_user = wp_get_current_user();
+			$roles        = $current_user->roles;
+			$role_style   = '';
+			foreach ( $roles as $role ) {
+				$role_style .= '.sme-hidden-by-role--' . $role . '{opacity: .3}';
+			}
+			if ( $role_style ) {
+				wp_add_inline_style( 'snow-monkey-editor@editor', $role_style );
+			}
 		}
 	}
 
@@ -59,58 +114,6 @@ class Assets {
 	}
 
 	/**
-	 * Enqueue editor assets
-	 */
-	public function _enqueue_block_editor_assets() {
-		$asset = include SNOW_MONKEY_EDITOR_PATH . '/dist/js/editor.asset.php';
-		wp_enqueue_script(
-			'snow-monkey-editor@editor',
-			SNOW_MONKEY_EDITOR_URL . '/dist/js/editor.js',
-			$asset['dependencies'],
-			filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/js/editor.js' ),
-			array(
-				'in_footer' => false,
-				'strategy'  => 'defer',
-			)
-		);
-
-		wp_set_script_translations(
-			'snow-monkey-editor@editor',
-			'snow-monkey-editor',
-			SNOW_MONKEY_EDITOR_PATH . '/languages'
-		);
-
-		wp_enqueue_style(
-			'snow-monkey-editor@editor',
-			SNOW_MONKEY_EDITOR_URL . '/dist/css/editor.css',
-			array(),
-			filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/css/editor.css' )
-		);
-
-		$palette     = get_theme_support( 'editor-color-palette' );
-		$color_style = '';
-		if ( is_array( $palette ) && ! empty( $palette ) ) {
-			foreach ( $palette[0] as $color ) {
-				$color_style .= ':root .editor-styles-wrapper .has-' . esc_attr( $color['slug'] ) . '-color[class*="sme-"],:root .customize-control-sidebar_block_editor .has-' . esc_attr( $color['slug'] ) . '-color[class*="sme-"]{color:' . esc_attr( $color['color'] ) . '}';
-				$color_style .= ':root .editor-styles-wrapper .has-' . esc_attr( $color['slug'] ) . '-background-color[class*="sme-"],:root .customize-control-sidebar_block_editor .has-' . esc_attr( $color['slug'] ) . '-background-color[class*="sme-"]{background-color:' . esc_attr( $color['color'] ) . '}';
-			}
-			if ( $color_style ) {
-				wp_add_inline_style( 'snow-monkey-editor@editor', $color_style );
-			}
-		}
-
-		$current_user = wp_get_current_user();
-		$roles        = $current_user->roles;
-		$role_style   = '';
-		foreach ( $roles as $role ) {
-			$role_style .= '.sme-hidden-by-role--' . $role . '{opacity: .3}';
-		}
-		if ( $role_style ) {
-			wp_add_inline_style( 'snow-monkey-editor@editor', $role_style );
-		}
-	}
-
-	/**
 	 * Enqueue front assets
 	 */
 	public function _wp_enqueue_scripts() {
@@ -123,13 +126,6 @@ class Assets {
 				'in_footer' => false,
 				'strategy'  => 'defer',
 			)
-		);
-
-		wp_enqueue_style(
-			'snow-monkey-editor',
-			SNOW_MONKEY_EDITOR_URL . '/dist/css/app.css',
-			array(),
-			filemtime( SNOW_MONKEY_EDITOR_PATH . '/dist/css/app.css' )
 		);
 	}
 }
