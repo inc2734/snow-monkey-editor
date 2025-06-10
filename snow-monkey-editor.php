68f9445d154df334c2a1dbeb31b06ea5ff78741d
inc2734 <inc@2inc.org>
2025-06-10 11:46:53 +0900

    11.0.2

--- snow-monkey-editor.php
+++ snow-monkey-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey Editor
- * Version: 11.0.1
+ * Version: 11.0.2
  * Description: Extends gutenberg block editor
  * Author: inc2734
  * Author URI: https://2inc.org
